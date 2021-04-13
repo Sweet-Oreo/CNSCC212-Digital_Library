9189e761045b332351dc55a34b86a71f025940f1
AreSZerA <notlichking@gmail.com>
2021-04-13 21:09:46 +0800

    Fixed a little problem.

--- src/web/AjaxCheckEmail.kt
+++ src/web/AjaxCheckEmail.kt
@@ -9,11 +9,7 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @WebServlet("/servlet/AjaxCheckEmail")
-<<<<<<< HEAD
 class AjaxCheckEmail : HttpServlet() {
-=======
-class RegisterAjaxServlet : HttpServlet() {
->>>>>>> Resolved conflicts.
 
     override fun doPost(req: HttpServletRequest?, resp: HttpServletResponse?) {
         val identity = req?.getParameter("identity").toString()
@@ -22,17 +18,6 @@ class RegisterAjaxServlet : HttpServlet() {
         when (identity) {
             "university" -> {
                 val universityService = UniversityServiceImpl()
-<<<<<<< HEAD
-                writer?.write(if (universityService.checkEmail(email)) "succeed" else "failed")
-            }
-            "reviewer" -> {
-                val reviewerService = ReviewerServiceImpl()
-                writer?.write(if (reviewerService.checkEmail(email)) "succeed" else "failed")
-            }
-            "reader" -> {
-                val readerService = ReaderServiceImpl()
-                writer?.write(if (readerService.checkEmail(email)) "succeed" else "failed")
-=======
                 writer?.write(if (universityService.checkUniversityEmail(email)) "succeed" else "failed")
             }
             "reviewer" -> {
@@ -42,7 +27,6 @@ class RegisterAjaxServlet : HttpServlet() {
             "reader" -> {
                 val readerService = ReaderServiceImpl()
                 writer?.write(if (readerService.checkReaderEmail(email)) "succeed" else "failed")
->>>>>>> Resolved conflicts.
             }
             else -> {
                 writer?.write("failed")
